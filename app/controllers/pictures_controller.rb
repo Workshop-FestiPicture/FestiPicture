c9307566535871ddf5e1d86a140c48c0135069fd
Gonthier Antoine <antoinegonthier@hotmail.com>
2014-12-11 01:42:31 +0100

    pictures.controller deleted

--- app/controllers/pictures_controller.rb
+++
@@ -1,9 +0,0 @@
-class PictureController < ActionController::Base
-
-	def code_image
-	  @image_data = Picture.find(params[:id])
-	  @image = @image_data.picture
-	  send_data (@image, :type     => "image/jpeg",
-	                     :disposition => 'inline')
-	end
-end
